f51bf7b32d77bd940db7178d2b3baeee0ebfff6e
david <davidmovsichoff@gmail.com>
2023-04-12 20:44:20 -0300

    ejercicio 10 modulos

---
+++ ejercicio-10/main.py
@@ -0,0 +1,10 @@
+import operations as op
+
+def main():
+    print(op.addition(2,2))
+    print(op.substraction(10,4))
+    print(op.multiplication(3,3))
+    print(op.division(21, 3))
+
+if (__name__ == '__main__'):
+    main()
