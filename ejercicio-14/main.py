cbcc6458b1660fe04c59ed069eb4203972e4dcdd
David <davidmovsichoff@gmail.com>
2023-05-02 18:55:53 -0300

    fix: sorted function

--- ejercicio-14/main.py
+++ ejercicio-14/main.py
@@ -1,2 +1,4 @@
-countries = input('Enter the country names separated by commas')
-print(set(countries.split(',').sort()))
+countries = input('Enter the country names separated by commas: ')
+countries_set = sorted(set(countries.split(',')))
+print('List of countries alphabetically ordered and without duplicates: \n',
+    countries_set)
